130ebc502bf1b44270d4f23406c8334c8bcb8f44
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:16:07 +0900

    Create CLA02_ADC_Result_Acq.d

---
+++ Debug/Source/CLA02_ADC_Result_Acq.d
@@ -0,0 +1,141 @@
+# FIXED
+
+Source/CLA02_ADC_Result_Acq.obj: ../Source/CLA02_ADC_Result_Acq.cla
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/CLA02_ADC_Result_Acq-CLA_Shared.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F28x_Project.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Cla_typedefs.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_device.h
+Source/CLA02_ADC_Result_Acq.obj: C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h
+Source/CLA02_ADC_Result_Acq.obj: C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h
+Source/CLA02_ADC_Result_Acq.obj: C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_adc.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_analogsubsys.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_can.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cla.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cmpss.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cputimer.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dac.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dcsm.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dma.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_ecap.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_emif.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_epwm.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_epwm_xbar.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_eqep.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_flash.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_gpio.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_i2c.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_input_xbar.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_mcbsp.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_memconfig.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_nmiintrupt.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_output_xbar.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_piectrl.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_pievect.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sci.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sdfm.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_spi.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sysctrl.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_upp.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_xbar.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_xint.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/cc.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/dac.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_device.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/function.h
+Source/CLA02_ADC_Result_Acq.obj: C:/ti/controlSUITE/libs/math/CLAmath/v400/include/CLAmath.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/variable.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/adc.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/fault.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/epwm.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/gpio.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/da.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/SFO_V8.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Examples.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_GlobalPrototypes.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cputimervars.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Cla_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_EPwm_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Adc_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Emif_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Gpio_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_I2c_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Pie_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Dma_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_SysCtrl_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Upp_defines.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_defaultisr.h
+Source/CLA02_ADC_Result_Acq.obj: C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F28x_Project.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/ADCDRV_F_C.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/DPlib.h
+Source/CLA02_ADC_Result_Acq.obj: C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/ADCDRV_F_C.h
+
+../Source/CLA02_ADC_Result_Acq.cla: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/CLA02_ADC_Result_Acq-CLA_Shared.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F28x_Project.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Cla_typedefs.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_device.h: 
+C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h: 
+C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h: 
+C:/ti/ccsv6.2/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_adc.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_analogsubsys.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_can.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cla.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cmpss.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cputimer.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dac.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dcsm.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_dma.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_ecap.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_emif.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_epwm.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_epwm_xbar.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_eqep.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_flash.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_gpio.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_i2c.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_input_xbar.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_mcbsp.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_memconfig.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_nmiintrupt.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_output_xbar.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_piectrl.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_pievect.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sci.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sdfm.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_spi.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_sysctrl.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_upp.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_xbar.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_xint.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/cc.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/dac.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_device.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/function.h: 
+C:/ti/controlSUITE/libs/math/CLAmath/v400/include/CLAmath.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/variable.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/adc.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/fault.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/epwm.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/gpio.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/da.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/SFO_V8.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Examples.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_GlobalPrototypes.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_cputimervars.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Cla_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_EPwm_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Adc_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Emif_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Gpio_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_I2c_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Pie_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Dma_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_SysCtrl_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_Upp_defines.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/F2837xS_defaultisr.h: 
+C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_common/include/F28x_Project.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/ADCDRV_F_C.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/DPlib.h: 
+C:/Users/UoS_PEETS/workspace_v6_0/wpt_v4/Include/ADCDRV_F_C.h: 
