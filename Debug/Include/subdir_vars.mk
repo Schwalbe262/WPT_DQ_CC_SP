c3bb64bbfd929fe40df6c146d11fe06869eb004b
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:10:20 +0900

    Create subdir_vars.mk

---
+++ Debug/Include/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LIB_SRCS += \
+../Include/cla1_math_library_fpu32.lib 
+
+ASM_SRCS += \
+../Include/CLASinCosTable.asm 
+
+OBJS += \
+./Include/CLASinCosTable.obj 
+
+ASM_DEPS += \
+./Include/CLASinCosTable.d 
+
+OBJS__QUOTED += \
+"Include\CLASinCosTable.obj" 
+
+ASM_DEPS__QUOTED += \
+"Include\CLASinCosTable.d" 
+
+ASM_SRCS__QUOTED += \
+"../Include/CLASinCosTable.asm" 
+
+
