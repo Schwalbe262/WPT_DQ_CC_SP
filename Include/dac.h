4bd31d42dd6c1ccebc8196ac00cb3b9617eadce8
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:14:06 +0900

    Create dac.h

---
+++ Include/dac.h
@@ -0,0 +1,15 @@
+/*
+ * dac.h
+ *
+ *  Created on: 2017. 1. 23.
+ *      Author: UoS_PEETS
+ */
+#include "F2837xS_device.h"
+
+#ifndef SOURCE_DAC_H_
+#define SOURCE_DAC_H_
+
+void ConfigureDAC(void);
+
+
+#endif /* SOURCE_DAC_H_ */
