200f9d2bc514afef17a772eb82ecfbcd19249cea
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:12:51 +0900

    Create gpio.h

---
+++ Include/gpio.h
@@ -0,0 +1,19 @@
+/*
+ * gpio.h
+ *
+ *  Created on: 2017. 1. 25.
+ *      Author: UoS_PEETS
+ */
+
+#ifndef INCLUDE_GPIO_H_
+#define INCLUDE_GPIO_H_
+
+#define PWM_EN0 88
+#define PWM_EN1 89
+
+extern void Configure_LED_GPIO();
+extern void Configure_PWM_EN_GPIO();
+extern void Configure_HW_PROT_GPIO();
+
+
+#endif /* INCLUDE_GPIO_H_ */
