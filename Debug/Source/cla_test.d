e0e1c90df4d445571217697226e60a702bd10325
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:10:34 +0900

    Create cla_test.d

---
+++ Debug/Source/cla_test.d
@@ -0,0 +1,129 @@
+# FIXED
+
+Source/cla_test.obj: ../Source/cla_test.cla
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/cla_test.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F28x_Project.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Cla_typedefs.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_device.h
+Source/cla_test.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h
+Source/cla_test.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h
+Source/cla_test.obj: C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_adc.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_analogsubsys.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cla.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cmpss.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cputimer.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dac.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dcsm.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dma.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_ecap.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_emif.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_epwm.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_epwm_xbar.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_eqep.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_flash.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_gpio.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_i2c.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_input_xbar.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_mcbsp.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_memconfig.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_nmiintrupt.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_output_xbar.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_piectrl.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_pievect.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sci.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sdfm.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_spi.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sysctrl.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_upp.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_xbar.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_xint.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/cc.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/function.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_device.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/CLAmath.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/variable.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/adc.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/fault.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/epwm.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/gpio.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/da.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/SFO_V8.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Examples.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_GlobalPrototypes.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cputimervars.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Cla_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_EPwm_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Adc_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Emif_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Gpio_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_I2c_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Pie_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Dma_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_SysCtrl_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Upp_defines.h
+Source/cla_test.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_defaultisr.h
+
+../Source/cla_test.cla: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/cla_test.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F28x_Project.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Cla_typedefs.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_device.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stddef.h: 
+C:/ti/ccsv6/tools/compiler/c2000_15.12.3.LTS/include/stdint.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_adc.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_analogsubsys.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cla.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cmpss.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cputimer.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dac.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dcsm.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_dma.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_ecap.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_emif.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_epwm.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_epwm_xbar.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_eqep.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_flash.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_gpio.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_i2c.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_input_xbar.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_mcbsp.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_memconfig.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_nmiintrupt.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_output_xbar.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_piectrl.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_pievect.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sci.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sdfm.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_spi.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_sysctrl.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_upp.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_xbar.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_xint.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/cc.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/function.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_device.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/CLAmath.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/variable.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/adc.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/fault.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/epwm.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/gpio.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/da.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/SFO_V8.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Examples.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_GlobalPrototypes.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_cputimervars.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Cla_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_EPwm_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Adc_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Emif_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Gpio_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_I2c_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Pie_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Dma_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_SysCtrl_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_Upp_defines.h: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/F2837xS_defaultisr.h: 
