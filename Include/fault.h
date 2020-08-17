224f0a0619e17c2b9592ea2e4f784050803d3f62
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:12:58 +0900

    Create fault.h

---
+++ Include/fault.h
@@ -0,0 +1,17 @@
+/*
+ * fault.h
+ *
+ *  Created on: 2017. 1. 24.
+ *      Author: UoS_PEETS
+ */
+
+#ifndef INCLUDE_FAULT_H_
+#define INCLUDE_FAULT_H_
+
+extern interrupt void hw_fault1(void);
+extern interrupt void hw_fault2(void);
+extern interrupt void hw_fault3(void);
+extern interrupt void hw_fault4(void);
+extern interrupt void hw_fault5(void);
+
+#endif /* INCLUDE_FAULT_H_ */
