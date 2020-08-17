2fefe424687a2d0b95825d1087420457b47caf1b
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:14:56 +0900

    Create subdir_vars.mk

---
+++ Debug/Source/subdir_vars.mk
@@ -0,0 +1,259 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../Source/F2837xS_CodeStartBranch.asm \
+../Source/F2837xS_DBGIER.asm \
+../Source/F2837xS_usDelay.asm 
+
+CLA_SRCS += \
+../Source/cla_test.cla 
+
+C_SRCS += \
+../Source/F2837xS_Adc.c \
+../Source/F2837xS_CpuTimers.c \
+../Source/F2837xS_DefaultISR.c \
+../Source/F2837xS_Dma.c \
+../Source/F2837xS_ECap.c \
+../Source/F2837xS_EPwm.c \
+../Source/F2837xS_EQep.c \
+../Source/F2837xS_GlobalVariableDefs.c \
+../Source/F2837xS_Gpio.c \
+../Source/F2837xS_I2C.c \
+../Source/F2837xS_Mcbsp.c \
+../Source/F2837xS_PieCtrl.c \
+../Source/F2837xS_PieVect.c \
+../Source/F2837xS_Sci.c \
+../Source/F2837xS_Spi.c \
+../Source/F2837xS_SysCtrl.c \
+../Source/F2837xS_TempSensorConv.c \
+../Source/F2837xS_Upp.c \
+../Source/F2837xS_can.c \
+../Source/F2837xS_sci_io.c \
+../Source/F2837xS_sdfm_drivers.c \
+../Source/F2837xS_struct.c \
+../Source/RingBuff.c \
+../Source/adc.c \
+../Source/cc.c \
+../Source/da.c \
+../Source/dac.c \
+../Source/easy2837xS_sci_v8.5.c \
+../Source/epwm.c \
+../Source/fault.c \
+../Source/gpio.c \
+../Source/main.c \
+../Source/mathtable.c \
+../Source/variable.c 
+
+OBJS += \
+./Source/F2837xS_Adc.obj \
+./Source/F2837xS_CodeStartBranch.obj \
+./Source/F2837xS_CpuTimers.obj \
+./Source/F2837xS_DBGIER.obj \
+./Source/F2837xS_DefaultISR.obj \
+./Source/F2837xS_Dma.obj \
+./Source/F2837xS_ECap.obj \
+./Source/F2837xS_EPwm.obj \
+./Source/F2837xS_EQep.obj \
+./Source/F2837xS_GlobalVariableDefs.obj \
+./Source/F2837xS_Gpio.obj \
+./Source/F2837xS_I2C.obj \
+./Source/F2837xS_Mcbsp.obj \
+./Source/F2837xS_PieCtrl.obj \
+./Source/F2837xS_PieVect.obj \
+./Source/F2837xS_Sci.obj \
+./Source/F2837xS_Spi.obj \
+./Source/F2837xS_SysCtrl.obj \
+./Source/F2837xS_TempSensorConv.obj \
+./Source/F2837xS_Upp.obj \
+./Source/F2837xS_can.obj \
+./Source/F2837xS_sci_io.obj \
+./Source/F2837xS_sdfm_drivers.obj \
+./Source/F2837xS_struct.obj \
+./Source/F2837xS_usDelay.obj \
+./Source/RingBuff.obj \
+./Source/adc.obj \
+./Source/cc.obj \
+./Source/cla_test.obj \
+./Source/da.obj \
+./Source/dac.obj \
+./Source/easy2837xS_sci_v8.5.obj \
+./Source/epwm.obj \
+./Source/fault.obj \
+./Source/gpio.obj \
+./Source/main.obj \
+./Source/mathtable.obj \
+./Source/variable.obj 
+
+ASM_DEPS += \
+./Source/F2837xS_CodeStartBranch.d \
+./Source/F2837xS_DBGIER.d \
+./Source/F2837xS_usDelay.d 
+
+CLA_DEPS += \
+./Source/cla_test.d 
+
+C_DEPS += \
+./Source/F2837xS_Adc.d \
+./Source/F2837xS_CpuTimers.d \
+./Source/F2837xS_DefaultISR.d \
+./Source/F2837xS_Dma.d \
+./Source/F2837xS_ECap.d \
+./Source/F2837xS_EPwm.d \
+./Source/F2837xS_EQep.d \
+./Source/F2837xS_GlobalVariableDefs.d \
+./Source/F2837xS_Gpio.d \
+./Source/F2837xS_I2C.d \
+./Source/F2837xS_Mcbsp.d \
+./Source/F2837xS_PieCtrl.d \
+./Source/F2837xS_PieVect.d \
+./Source/F2837xS_Sci.d \
+./Source/F2837xS_Spi.d \
+./Source/F2837xS_SysCtrl.d \
+./Source/F2837xS_TempSensorConv.d \
+./Source/F2837xS_Upp.d \
+./Source/F2837xS_can.d \
+./Source/F2837xS_sci_io.d \
+./Source/F2837xS_sdfm_drivers.d \
+./Source/F2837xS_struct.d \
+./Source/RingBuff.d \
+./Source/adc.d \
+./Source/cc.d \
+./Source/da.d \
+./Source/dac.d \
+./Source/easy2837xS_sci_v8.5.d \
+./Source/epwm.d \
+./Source/fault.d \
+./Source/gpio.d \
+./Source/main.d \
+./Source/mathtable.d \
+./Source/variable.d 
+
+CLA_DEPS__QUOTED += \
+"Source\cla_test.d" 
+
+C_DEPS__QUOTED += \
+"Source\F2837xS_Adc.d" \
+"Source\F2837xS_CpuTimers.d" \
+"Source\F2837xS_DefaultISR.d" \
+"Source\F2837xS_Dma.d" \
+"Source\F2837xS_ECap.d" \
+"Source\F2837xS_EPwm.d" \
+"Source\F2837xS_EQep.d" \
+"Source\F2837xS_GlobalVariableDefs.d" \
+"Source\F2837xS_Gpio.d" \
+"Source\F2837xS_I2C.d" \
+"Source\F2837xS_Mcbsp.d" \
+"Source\F2837xS_PieCtrl.d" \
+"Source\F2837xS_PieVect.d" \
+"Source\F2837xS_Sci.d" \
+"Source\F2837xS_Spi.d" \
+"Source\F2837xS_SysCtrl.d" \
+"Source\F2837xS_TempSensorConv.d" \
+"Source\F2837xS_Upp.d" \
+"Source\F2837xS_can.d" \
+"Source\F2837xS_sci_io.d" \
+"Source\F2837xS_sdfm_drivers.d" \
+"Source\F2837xS_struct.d" \
+"Source\RingBuff.d" \
+"Source\adc.d" \
+"Source\cc.d" \
+"Source\da.d" \
+"Source\dac.d" \
+"Source\easy2837xS_sci_v8.5.d" \
+"Source\epwm.d" \
+"Source\fault.d" \
+"Source\gpio.d" \
+"Source\main.d" \
+"Source\mathtable.d" \
+"Source\variable.d" 
+
+OBJS__QUOTED += \
+"Source\F2837xS_Adc.obj" \
+"Source\F2837xS_CodeStartBranch.obj" \
+"Source\F2837xS_CpuTimers.obj" \
+"Source\F2837xS_DBGIER.obj" \
+"Source\F2837xS_DefaultISR.obj" \
+"Source\F2837xS_Dma.obj" \
+"Source\F2837xS_ECap.obj" \
+"Source\F2837xS_EPwm.obj" \
+"Source\F2837xS_EQep.obj" \
+"Source\F2837xS_GlobalVariableDefs.obj" \
+"Source\F2837xS_Gpio.obj" \
+"Source\F2837xS_I2C.obj" \
+"Source\F2837xS_Mcbsp.obj" \
+"Source\F2837xS_PieCtrl.obj" \
+"Source\F2837xS_PieVect.obj" \
+"Source\F2837xS_Sci.obj" \
+"Source\F2837xS_Spi.obj" \
+"Source\F2837xS_SysCtrl.obj" \
+"Source\F2837xS_TempSensorConv.obj" \
+"Source\F2837xS_Upp.obj" \
+"Source\F2837xS_can.obj" \
+"Source\F2837xS_sci_io.obj" \
+"Source\F2837xS_sdfm_drivers.obj" \
+"Source\F2837xS_struct.obj" \
+"Source\F2837xS_usDelay.obj" \
+"Source\RingBuff.obj" \
+"Source\adc.obj" \
+"Source\cc.obj" \
+"Source\cla_test.obj" \
+"Source\da.obj" \
+"Source\dac.obj" \
+"Source\easy2837xS_sci_v8.5.obj" \
+"Source\epwm.obj" \
+"Source\fault.obj" \
+"Source\gpio.obj" \
+"Source\main.obj" \
+"Source\mathtable.obj" \
+"Source\variable.obj" 
+
+ASM_DEPS__QUOTED += \
+"Source\F2837xS_CodeStartBranch.d" \
+"Source\F2837xS_DBGIER.d" \
+"Source\F2837xS_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"../Source/F2837xS_Adc.c" \
+"../Source/F2837xS_CpuTimers.c" \
+"../Source/F2837xS_DefaultISR.c" \
+"../Source/F2837xS_Dma.c" \
+"../Source/F2837xS_ECap.c" \
+"../Source/F2837xS_EPwm.c" \
+"../Source/F2837xS_EQep.c" \
+"../Source/F2837xS_GlobalVariableDefs.c" \
+"../Source/F2837xS_Gpio.c" \
+"../Source/F2837xS_I2C.c" \
+"../Source/F2837xS_Mcbsp.c" \
+"../Source/F2837xS_PieCtrl.c" \
+"../Source/F2837xS_PieVect.c" \
+"../Source/F2837xS_Sci.c" \
+"../Source/F2837xS_Spi.c" \
+"../Source/F2837xS_SysCtrl.c" \
+"../Source/F2837xS_TempSensorConv.c" \
+"../Source/F2837xS_Upp.c" \
+"../Source/F2837xS_can.c" \
+"../Source/F2837xS_sci_io.c" \
+"../Source/F2837xS_sdfm_drivers.c" \
+"../Source/F2837xS_struct.c" \
+"../Source/RingBuff.c" \
+"../Source/adc.c" \
+"../Source/cc.c" \
+"../Source/da.c" \
+"../Source/dac.c" \
+"../Source/easy2837xS_sci_v8.5.c" \
+"../Source/epwm.c" \
+"../Source/fault.c" \
+"../Source/gpio.c" \
+"../Source/main.c" \
+"../Source/mathtable.c" \
+"../Source/variable.c" 
+
+ASM_SRCS__QUOTED += \
+"../Source/F2837xS_CodeStartBranch.asm" \
+"../Source/F2837xS_DBGIER.asm" \
+"../Source/F2837xS_usDelay.asm" 
+
+
