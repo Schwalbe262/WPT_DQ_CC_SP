3b468203708d57f9f0284d6e9b8cb211cf19d452
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:10:24 +0900

    Create objects.mk

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/ti/controlSUITE/device_support/f28m36x/v209/F28M36x_common/lib/SFO_TI_Build_V7_FPU.lib" -l"C:/ti/controlSUITE/libs/math/CLAmath/v4_02_00_00/lib/cla0_math_library.lib" -l"C:/ti/controlSUITE/device_support/F2837xS/v200/F2837xS_common/cmd/2837xS_RAM_CLA_lnk_cpu1.cmd" -l"C:/ti/controlSUITE/libs/math/CLAmath/v4_02_00_00/lib/cla0_math_library_fpu32.lib" -l"C:/ti/controlSUITE/device_support/F2837xS/v210/F2837xS_headers/cmd/F2837xS_Headers_nonBIOS.cmd" -lrts2800_fpu32.lib
+
