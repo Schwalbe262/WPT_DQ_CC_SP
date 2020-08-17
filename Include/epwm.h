80ef6f38f262a1dbde22df451d2d3ae638690a30
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:14:02 +0900

    Create epwm.h

---
+++ Include/epwm.h
@@ -0,0 +1,23 @@
+/*
+ * epwm.h
+ *
+ *  Created on: 2017. 1. 24.
+ *      Author: UoS_PEETS
+ */
+
+#ifndef INCLUDE_EPWM_H_
+#define INCLUDE_EPWM_H_
+
+
+extern void ConfigureEPWM1(void);
+extern void ConfigureEPWM2(void);
+extern void ConfigureEPWM3(void);
+extern void ConfigureEPWM4(void);
+extern void ConfigureEPWM5(void);
+extern void ConfigureEPWM6(void);
+extern void ConfigureEPWM7(void);
+
+
+
+
+#endif /* INCLUDE_EPWM_H_ */
