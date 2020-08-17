21d882d0f3801e62cd995556562726f06357086d
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:14:20 +0900

    Create adc.h

---
+++ Include/adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ *  Created on: 2017. 1. 23.
+ *      Author: UoS_PEETS
+ */
+
+#ifndef INCLUDE_ADC_H_
+#define INCLUDE_ADC_H_
+
+
+void ConfigureADC(void);
+
+
+
+#endif /* INCLUDE_ADC_H_ */
