25f268a562de8778ec4e12e6e58eb23fa45f0c04
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:13:04 +0900

    Create mathtable.h

---
+++ Include/mathtable.h
@@ -0,0 +1,16 @@
+/*
+ * mathtable.h
+ *
+ *  Created on: 2019. 3. 8.
+ *      Author: PEETS
+ */
+
+#ifndef INCLUDE_MATHTABLE_H_
+#define INCLUDE_MATHTABLE_H_
+
+extern float asin_val[201];
+extern float cos_val[201];
+
+
+
+#endif /* INCLUDE_MATHTABLE_H_ */
