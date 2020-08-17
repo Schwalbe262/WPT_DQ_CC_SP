9f6f2943c0dd11ab0fcadc15d532e037854052c5
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:12:24 +0900

    Create da.c

---
+++ Source/da.c
@@ -0,0 +1,17 @@
+unsigned int
+        da1                     =0,
+        da2                     =0,     
+        da3                     =0,     
+        da1_type                =0,     
+        da2_type                =0,     
+        da3_type                =0;
+
+float   da1_rng                 =0,    
+        da1_val                 =0,    
+		da1_mid					=0,	
+        da2_rng                 =0,    
+        da2_val                 =0,    
+		da2_mid					=0,	
+		da3_rng                 =0,    
+        da3_val                 =0,    
+		da3_mid					=0;
