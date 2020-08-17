a085f23bce7fecf5883b6bbe78c6e63ca7d00de6
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:14:52 +0900

    Create subdir_vars.mk

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xS_RAM_CLA_lnk_cpu1.cmd 
+
+
