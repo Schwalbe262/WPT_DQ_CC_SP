dd810eb24160546a4b661ed80e16159dc49669d6
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2020-08-17 18:15:01 +0900

    Create RingBuff.d

---
+++ Debug/Source/RingBuff.d
@@ -0,0 +1,7 @@
+# FIXED
+
+Source/RingBuff.obj: ../Source/RingBuff.c
+Source/RingBuff.obj: C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/RingBuff.h
+
+../Source/RingBuff.c: 
+C:/Users/PEETS/workspace_polar/wpt_cla_NEC/Include/RingBuff.h: 
